a17705f6280a5d953bffad208abdd0bcdf83b947
simantabarua <webdevelopersimanta@gmil.com>
2023-01-01 00:11:26 +0600

    new

--- custom.js
+++ custom.js
@@ -19,3 +19,47 @@ setInterval(() => {
     textIndex = (textIndex + 1) % messages.length;
   }, 500);
 }, 1000);
+
+<script type="module">
+
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyC20NS96bFTBrGO0xLjKF2ctpakZ6dvE80",
+
+  authDomain: "newyar-aaf1b.firebaseapp.com",
+
+  projectId: "newyar-aaf1b",
+
+  storageBucket: "newyar-aaf1b.appspot.com",
+
+  messagingSenderId: "527743328769",
+
+  appId: "1:527743328769:web:e40ce908efbcb2da6d9600",
+
+  measurementId: "G-87J8ETRGQF"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const analytics = getAnalytics(app);
+
+</script>
